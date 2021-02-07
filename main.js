f30bdb138591c4d72b3fbba702570bd936cb3da7
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-02-07 10:46:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,15 +11,27 @@ recognition.onresult=function(e){
     var content=e.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
-    speak();
+    if(content=="take my selfie") {
+        speak();
+        console.log("take my selfie");
+    }else{
+        console.log("say take my selfie to take selfie");
+    }
 }
 
 function speak() {
     var synth=window.speechSynthesis;
-    var speakdata=document.getElementById("textbox").value;
+    var speakdata="taking your selfie in 5 seconds";
     var Utter=new SpeechSynthesisUtterance(speakdata);
     synth.speak(Utter);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
+
+camera=document.getElementById("camera");
 
 Webcam.set({
     width:360,
@@ -27,6 +39,17 @@ Webcam.set({
     image_format:'png',
     png_quality:100
 });
-camera=document.getElementById("camera");
-Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function(img_url){
+        document.getElementById("result").innerHTML="<img id='capturedimage' src='"+img_url+"'>";
+    });
+}
+
+function save() {
+    link=document.getElementById("link");
+    img=document.getElementById("capturedimage");
+    link.href=img;
+    link.click();
+}
 
